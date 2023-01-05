e57e4d955f8172ad7fab9019637473e70cfed7d1
plamyaa <sergeevm100@gmail.com>
2023-01-05 23:23:18 +0300

    added filtering by brand, collection or author

--- server/news/viewsets.py
+++ server/news/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import News
 from .serializers import NewsSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author_id', 'brand_id', 'collection_id']
